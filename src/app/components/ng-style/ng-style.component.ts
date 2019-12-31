2ba5f6c31fc307fd52f3af8f6c2b7b53919b66cc
Juan Ruiz <jaruizg_1@uqvirtual.edu.co>
2019-12-30 21:44:15 -0500

    version 1

---
+++ src/app/components/ng-style/ng-style.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-style',
+  template: `
+    <p [ngStyle]="{'font-size.px': tamanio}">
+      Tamaño de fuente: {{tamanio}}
+    </p>
+
+    <button class="btn btn-primary" (click)="aumentarFuente()">
+      <i class="fas fa-plus"></i>
+    </button>
+
+    <button class="btn btn-primary" (click)="disminuirFuente()">
+      <i class="fas fa-minus"></i>
+    </button>
+  `,
+  styles: []
+})
+export class NgStyleComponent implements OnInit {
+
+  tamanio = 20;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  aumentarFuente() {
+    if ( this.tamanio < 50 ) {
+      this.tamanio += 5;
+    }
+  }
+
+  disminuirFuente() {
+    if ( this.tamanio > 5 ) {
+      this.tamanio -= 5;
+    }
+  }
+
+}
